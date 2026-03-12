9576ad1dabb40a4c846e287e6be2e5012400c27b
khushiparwal <parwalkhushi@gmail.com>
2026-03-12 11:58:20 +0530

    Done

--- sos/ors/urls.py
+++ sos/ors/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('test/',views.test_ors)
+    path('test/',views.test_ors),
+    path('welcome/',views.welcome),
+    path('signup/',views.signup),
 ]
--- sos/ors/views.py
+++ sos/ors/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 def test_ors(request):
     return HttpResponse('<h1>Django project ors</h1>')
 # Create your views here.
+def welcome(request):
+    return render(request, "welcome.html")
+
+def signup(request):
+    print(request.GET.get("Firstname"))
+    print(request.GET.get("Lastname"))
+    print(request.GET.get("Email"))
+    print(request.GET.get("DOB"))
+    print(request.GET.get("Address"))
+    return render(request, "registration.html")
